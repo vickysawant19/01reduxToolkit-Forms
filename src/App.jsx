a6c8eb4043fade0b38d61a65855d6650c8e52fc8
vickysawant19 <vicky.sawant19@gmail.com>
2024-02-21 00:04:14 +0530

    added user thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
 import Posts from "./features/posts/Posts";
+import { useEffect } from "react";
+import { fetchUsers, selectAllUsers } from "./features/users/usersSlice";
+import { getStatus } from "./features/posts/postSlice";
 
 function App() {
+  const users = useSelector(selectAllUsers);
+  const status = useSelector(getStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchUsers());
+    }
+  }, []);
+
   return (
     <>
       <div className="bg-gray-600 w-full h-full flex flex-col items-center pt-2 min-h-screen">
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,22 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  {
-    id: 1,
-    username: "vicky sawant",
-  },
-  {
-    id: 2,
-    username: "vitthal sawant",
-  },
-];
-
+const initialState = {
+  users: [],
+  status: "idle",
+  error: null,
+};
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+});
 export const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.status = "Success";
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.status = "error";
+        state.error = action.error;
+      })
+      .addCase(fetchUsers.pending, (state, action) => {
+        state.status = "Pending";
+      });
+  },
 });
 
 export const {} = usersSlice.actions;
-export const selectAllUsers = (state) => state.users;
+export const getStatus = (state) => state.users.status;
+export const getError = (state) => state.users.error;
+export const selectAllUsers = (state) => state.users.users;
 export default usersSlice.reducer;
